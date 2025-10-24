6e44430555154aa5711a7f009071167bdde5f59c
Phumipat Peamlue <ppbesto.9945@gmail.com>
2025-10-24 17:32:58 +0700

    add reset-game button and update game layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import Board from "./Board"
 import { GameProvider } from "./GameProvider"
+import ResetGameButton from "./ResetGameButton"
 
 export default function App() {
   return (
@@ -13,8 +14,9 @@ export default function App() {
         "https://images.unsplash.com/photo-1496483648148-47c686dc86a8",
         "https://images.unsplash.com/photo-1591181520189-abcb0735c65d",
       ]}>
-        <header>
+        <header className="header">
           <h1 className="title">Memory Game</h1>
+          <ResetGameButton>Reset</ResetGameButton>
         </header>
         <main>
           <Board />
